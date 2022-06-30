c49e9723404393710ee0074634d1b8e85b2687f6
mgciszewski <85741748+mgciszewski@users.noreply.github.com>
2022-06-30 10:36:46 +0200

    Calculate the LTS measure of the estimated labels

--- measures.py
+++ measures.py
@@ -1,7 +1,7 @@
 import numpy as np
 from misc_func import detect_jumps
 
-def lts_measure(correct_df, estimated_df, w = 0.6, sigma = 0.35, lam = 0.01, zeta = 0.5):
+def lts_measure(correct_df, estimated_df, fs = 1000, w = 0.6, sigma = 0.35, lam = 0.01, zeta = 0.5):
     """
     Calculates the LTS measure of the estimated labels.
 
@@ -11,6 +11,8 @@ def lts_measure(correct_df, estimated_df, w = 0.6, sigma = 0.35, lam = 0.01, zet
                  Series containing true labels
     estimated_df : pandas Series
                    Series containing estimated labels
+    fs : int
+         sampling frequency
     w : double
         controls the weight of misclassification occurring from the uncertainty of the true labels
     sigma : double
@@ -35,21 +37,22 @@ def lts_measure(correct_df, estimated_df, w = 0.6, sigma = 0.35, lam = 0.01, zet
         prev_pp_state = estimated_df[prev_jump]
         
         if prev_true_state != prev_pp_state:
-            if (abs(curr_jump - prev_jump) / 1000 <= sigma) & (curr_jump < 59999):
+            if (abs(curr_jump - prev_jump) / fs <= sigma) & (curr_jump < len(correct_df) - 1):
                 if (correct_df[prev_jump - 1] == estimated_df[prev_jump - 1]) & (correct_df[curr_jump + 1] == estimated_df[curr_jump + 1]):
-                    error += w * (curr_jump - prev_jump) / 1000
+                    error += w * (curr_jump - prev_jump) / fs
                 else:
-                    error += (curr_jump - prev_jump) / 1000
+                    error += (curr_jump - prev_jump) / fs
             else:
-                error += (curr_jump - prev_jump) / 1000
+                error += (curr_jump - prev_jump) / fs
         
         prev_jump = curr_jump
     
     penalty_term = 0
-    prev_jump = pp_jumps[0] / 1000
+    prev_jump = pp_jumps[0] / fs
     for curr_jump in pp_jumps[1:]:
-        if (curr_jump - prev_jump) / 1000 < zeta:
+        if (curr_jump - prev_jump) / fs < zeta:
             penalty_term += lam
-        prev_jump = curr_jump / 1000
+        prev_jump = curr_jump / fs
                 
-    return np.exp(-error / 60 - penalty_term)
+    max_seconds = len(correct_df) / fs
+    return np.exp(-error / max_seconds - penalty_term)
---
+++ misc_func.py
@@ -0,0 +1,188 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from random import choices
+
+def detect_jumps(l):
+    """
+    Detects all locations of the jumps in a list of numbers.
+
+    Parameters
+    ----------
+    l : array of numbers
+        an array of numbers to detect the locations of the jumps
+        
+    Returns
+    -------
+    an array consisting of all locations of the jumps in l
+    """
+    return [0] + [int(i) for i in range(1,len(l)) if l[i] != l[i-1]] + [len(l) - 1]
+    
+def flatten_list(l):
+    """
+    Flattens array of arrays; creates a single array with elements of all arrays in l.
+
+    Parameters
+    ----------
+    l : array of arrays
+        an array of arrays to flatten
+        
+    Returns
+    -------
+    an array consisting of elements from all arrays in l
+    """
+    return [item for sublist in l for item in sublist]
+
+def generate_state_seq(mu_1, mu_2):
+    """
+    Generate a state sequence of noisy labels as prescribed in section 4.1 of the manuscript.
+
+    Parameters
+    ----------
+    mu_1 : double
+           the parameter of the exponential distribution (time spent in the correct state)
+    mu_2 : double
+           the parameter of the exponential distribution (time spent in the incorrect state)
+        
+    Returns
+    -------
+    an array consisting of all locations of the jumps in l
+    """
+    total_time = 0
+    state_seq = []
+    incorrect_array = []
+    
+    jumps = []
+    
+    while total_time < 60000:
+        waiting_time = int(np.ceil(np.random.exponential(mu_1)))
+        if total_time + waiting_time > 60000:
+            waiting_time = 60000 - total_time
+        state_seq += [True for i in range(waiting_time)]
+        total_time += waiting_time
+        
+        jumps.append(total_time)
+
+        if total_time < 60000:
+            duration_time = int(np.ceil(np.random.exponential(mu_2)))
+            if total_time + duration_time > 59999:
+                duration_time = 59999 - total_time
+            
+            if total_time < 5000: # in state 1
+                incorrect = choices([2, 3])[0]
+            elif total_time < 15000: # in state 2
+                incorrect = choices([1, 3])[0]
+            elif total_time < 30000: # in state 3
+                incorrect = choices([1, 2])[0]
+            elif total_time < 40000: # in state 2
+                incorrect = choices([1, 3])[0]
+            elif total_time < 55000: # in state 3
+                incorrect = choices([1, 2])[0]
+            else: # in state 1
+                incorrect = choices([2, 3])[0]
+            total_time += duration_time
+        
+            jumps.append(total_time)
+                
+            state_seq += [False for i in range(duration_time)]
+            incorrect_array += [incorrect for i in range(duration_time)]
+    
+    return state_seq, incorrect_array
+
+def identify_jump_subseq(jumps, gam):
+    """
+    Returns array of arrays of jumps. 
+    Jumps are put into one block (array) if the distance between subsequent jumps in the block is smaller than gam.
+
+    Parameters
+    ----------
+    jumps : array of doubles
+        an array of jumps
+    gam : double
+        lower bound on the lengths of the intervals (also double the weight of the jump)
+
+    Returns
+    -------
+    res: an array of arrays of doubles
+        a list of all blocks of jumps such that each block contains jumps that have at least one gamma neighbour in it or the block is a singleton
+    """
+    # res is initialized to a list containing one empty array; last refers to the last jump considered; at this moment it's None
+    res, last = [[]], None
+    
+    # we iterate over all jumps
+    for x in jumps:
+        # if this is the first jump (last is None) or the previous jump was in gamma vicinity of 
+        # current jump x then x is added to the most recent block in res
+        if last is None or abs(last - x) < gam:
+            res[-1].append(x)
+        # otherwise a new block is created
+        else:
+            res.append([x])
+        # we update last
+        last = x
+    
+    return res
+
+def mystep(x,y, ax=None, **kwargs):
+    """ 
+    Plots discontinuous functions without vertical lines at jumps
+
+    Parameters
+    ----------
+    x: list
+        list of arguments
+    y: list
+        list of values
+    ax: axis object
+        Axis object on which the plot should appear
+
+    Returns
+    -------
+    the plot of y against xW
+    """
+    # convert x and y into numpy array
+    x = np.array(x)
+    y = np.array(y)
+    
+    X = np.c_[x[:-1],x[1:],x[1:]]
+    Y = np.c_[y[:-1],y[:-1],np.zeros_like(x[:-1])*np.nan]
+    
+    # if ax parameter is not given, use current figure
+    if not ax: ax=plt.gca()
+        
+    return ax.plot(X.flatten(), Y.flatten(), **kwargs)
+
+def set_size(width = 345.0, fraction=1, subplots=(1, 1)):
+    """ 
+    Set figure dimensions to avoid scaling in LaTeX.
+
+    Parameters
+    ----------
+    width: float
+        Document textwidth or columnwidth in pts
+    fraction: float, optional
+        Fraction of the width which you wish the figure to occupy
+
+    Returns
+    -------
+    fig_dim: tuple
+            Dimensions of figure in inches
+    """
+    # Width of figure (in pts)
+    fig_width_pt = width * fraction
+
+    # Convert from pt to inches
+    inches_per_pt = 1 / 72.27
+
+    # Golden ratio to set aesthetic figure height
+    golden_ratio = (5**.5 - 1) / 2
+
+    # Figure width in inches
+    fig_width_in = fig_width_pt * inches_per_pt
+    # Figure height in inches
+    fig_height_in = fig_width_in * golden_ratio * (subplots[0] / subplots[1])
+
+    # Dimensions of the figure as a tuple (in inches)
+    fig_dim = (fig_width_in, fig_height_in)
+
+    return fig_dim
